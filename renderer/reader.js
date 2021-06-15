5c861c27441815c107d5e6a88f67e5a8226258c3
PavelAvrusevich <p.avrusevich@gmail.com>
2021-06-15 16:39:14 +0300

    create Done button in remote content

--- renderer/reader.js
+++ renderer/reader.js
@@ -1,1 +1,20 @@
-alert('Hello from items.js');
+// Create "Done" button in remote content
+let closeButton = document.createElement('div');
+closeButton.innerText = 'Done';
+
+// Style button
+closeButton.style.position = 'fixed';
+closeButton.style.bottom = '15px';
+closeButton.style.right = '15px';
+closeButton.style.padding = '5px 10px';
+closeButton.style.fontSize = '20px';
+closeButton.style.fontWeight = 'bold';
+closeButton.style.background = 'dodgerblue';
+closeButton.style.color = 'white';
+closeButton.style.borderRadius = '5px';
+closeButton.style.cursor = 'default';
+closeButton.style.boxShadow = '2px 2px 2px rgba(0,0,0,0.2)';
+closeButton.style.zIndex = '9999';
+
+// Append button to body
+document.getElementsByTagName('body')[0].append(closeButton);
